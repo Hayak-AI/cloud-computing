c81cc8d53951ba04e9344619a0bbc4841f245b00
hwochaDev <salsabilaaprilia067@gmail.com>
2024-12-10 18:56:48 +0700

    morgan

--- src/server.js
+++ src/server.js
@@ -2,6 +2,15 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 const Jwt = require('@hapi/jwt');
+const morgan = require('morgan');
+const { Writable } = require('stream');
+
+const morganStream = new Writable({
+  write(chunk, encoding, callback) {
+    console.log(chunk.toString().trim());
+    callback();
+  },
+});
 
 const init = async () => {
   const server = Hapi.server({
@@ -34,10 +43,27 @@ const init = async () => {
     },
   });
 
+  server.ext('onRequest', (request, h) => {
+    morgan('combined', { stream: morganStream })(
+      request.raw.req,
+      request.raw.res,
+      (err) => {
+        if (err) {
+          console.error('Morgan logging error:', err);
+        }
+      },
+    );
+    return h.continue;
+  });
+
   server.route(routes);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
 
 init();
